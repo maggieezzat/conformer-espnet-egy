1ee5b9ae660c58e566430497671f99fcb2990ba1
maggieezzat <maggieezzat9@gmail.com>
2021-06-29 14:42:42 +0200

    fixed a small bug

--- asr/local/data.py
+++ asr/local/data.py
@@ -41,24 +41,24 @@ train_path = 'data/train'
 
 with open(os.path.join(train_path, 'wav.scp'), 'w') as wav_scp, open(os.path.join(train_path, 'text'), 'w') as text_f, open(os.path.join(train_path, 'utt2spk'), 'w') as utt2spk:
     for train_wave in train_waves:
-        wav_scp.write(train_wave[0] + ' ' + train_wave[1])
-        text_f.write(train_wave[0] + ' ' + train_wave[2])
-        utt2spk.write(train_wave[0] + ' ' + train_wave[0])
+        wav_scp.write(train_wave[0] + ' ' + train_wave[1] + '\n')
+        text_f.write(train_wave[0] + ' ' + train_wave[2] + '\n')
+        utt2spk.write(train_wave[0] + ' ' + train_wave[0] + '\n')
 
 dev_path = 'data/dev'
 
 with open(os.path.join(dev_path, 'wav.scp'), 'w') as wav_scp, open(os.path.join(dev_path, 'text'), 'w') as text_f, open(os.path.join(dev_path, 'utt2spk'), 'w') as utt2spk:
     for dev_wave in dev_waves:
-        wav_scp.write(dev_wave[0] + ' ' + dev_wave[1])
-        text_f.write(dev_wave[0] + ' ' + dev_wave[2])
-        utt2spk.write(dev_wave[0] + ' ' + dev_wave[0])
+        wav_scp.write(dev_wave[0] + ' ' + dev_wave[1] + '\n')
+        text_f.write(dev_wave[0] + ' ' + dev_wave[2] + '\n')
+        utt2spk.write(dev_wave[0] + ' ' + dev_wave[0] + '\n')
 
 
 test_path = 'data/test'
 
 with open(os.path.join(test_path, 'wav.scp'), 'w') as wav_scp, open(os.path.join(test_path, 'text'), 'w') as text_f, open(os.path.join(test_path, 'utt2spk'), 'w') as utt2spk:
     for test_wave in test_waves:
-        wav_scp.write(test_wave[0] + ' ' + test_wave[1])
-        text_f.write(test_wave[0] + ' ' + test_wave[2])
-        utt2spk.write(test_wave[0] + ' ' + test_wave[0])
+        wav_scp.write(test_wave[0] + ' ' + test_wave[1] + '\n')
+        text_f.write(test_wave[0] + ' ' + test_wave[2] + '\n')
+        utt2spk.write(test_wave[0] + ' ' + test_wave[0] + '\n')
 
